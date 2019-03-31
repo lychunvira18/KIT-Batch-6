2b13db124aeff37b76770aa7e1ce5c8deae74da7
lychunvira18 <lychunvira18@kit.edu.kh>
2019-03-31 16:39:57 +0700

    Update phone-number-horoscope.c
    Added footer function declarator

--- C Programming/General Applications/phone-number-horoscope.c
+++ C Programming/General Applications/phone-number-horoscope.c
@@ -3,45 +3,54 @@
 
 void header();
 void prompt();
+void footer();
 void horoscope();
 int calc();
-int main(){
-  header();
-  prompt();
-  horoscope();
-  footer();
-  return 0;
+int main()
+{
+    header();
+    prompt();
+    horoscope();
+    footer();
+    return 0;
 }
 
-void header(){
-  printf("=================================================\n   P H O N E   N U M B E R   H O R O S C O P E   \n=================================================\n");
+void header()
+{
+    printf("=================================================\n   P H O N E   N U M B E R   H O R O S C O P E   \n=================================================\n");
 }
 
-void footer(){
-  printf("\n=================================================\n=================================================\n");
+void footer()
+{
+    printf("\n=================================================\n=================================================\n");
 }
 
-void prompt(){
-  printf("\nEnter your phone number :   ");
+void prompt()
+{
+    printf("\nEnter your phone number :   ");
 }
 
-int calc(){
-  char pnStr[11];
-  int pnNum, sum=0;
-  scanf("%s",&pnStr);
-  pnNum = atoi(pnStr);
-  do{
-    sum = 0;
-    do{
-      sum+=pnNum%10;
-      pnNum/=10;
-    }while(pnNum>0);
-    pnNum = sum;
-  }while(sum>9);
+int calc()
+{
+    char pnStr[11];
+    int pnNum, sum = 0;
+    scanf("%s", &pnStr);
+    pnNum = atoi(pnStr);
+    do
+    {
+        sum = 0;
+        do
+        {
+            sum += pnNum % 10;
+            pnNum /= 10;
+        } while (pnNum > 0);
+        pnNum = sum;
+    } while (sum > 9);
 
-  return sum;
+    return sum;
 }
 
-void horoscope(){
-  printf("\nYour phone number horoscope result is : %d\n", calc());
+void horoscope()
+{
+    printf("\nYour phone number horoscope result is : %d\n", calc());
 }
